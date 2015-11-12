7abcd50928d9d805de4e0dcd3cfb3a8ec5fa05f6
Fredriko83 <folep02@student.lnu.se>
2015-11-12 23:54:57 +0100

    working on FB share button

---
+++ _includes/socialmedia.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Fredrik on 2015-11-12.
+ */
+<script>
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '1032606780136733',
+        xfbml      : true,
+        version    : 'v2.5'
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
